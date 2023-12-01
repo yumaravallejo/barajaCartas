ca101d97c1fbb80a013066e49425c462198e92dd
yvalval1411 <yvalval1411@g.educaand.es>
2023-12-01 10:03:09 +0100

    Constructor por defecto aleatorio

--- src/main/java/baraja/Naipes.java
+++ src/main/java/baraja/Naipes.java
@@ -14,7 +14,21 @@ public class Naipes {
     private int numCarta;
     private PaloCarta paloCarta;
 
-   
+    //Constructor por defecto y aleatorio
+    public Naipes() {
+        Random rd = new Random();
+        this.numCarta = rd.nextInt(1, 11);
+        int valoresRandom = rd.nextInt(PaloCarta.values().length);
+        if (valoresRandom == 1) {
+            this.paloCarta = PaloCarta.OROS;
+        } else if (valoresRandom == 2) {
+            this.paloCarta = PaloCarta.BASTOS;
+        } else if (valoresRandom == 3) {
+            this.paloCarta = PaloCarta.ESPADAS;
+        } else if (valoresRandom == 4) {
+            this.paloCarta = PaloCarta.COPAS;
+        }
+    }
 
 
     
